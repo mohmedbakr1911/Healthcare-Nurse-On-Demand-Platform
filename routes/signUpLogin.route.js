2c7e1a165565fdc8a9b9cfa75d48c5d99789a95c
Mohamed Bakr <mv7788277@gmail.com>
2025-09-09 21:34:04 +0300

    Login and register apis

--- routes/signUpLogin.route.js
+++ routes/signUpLogin.route.js
@@ -4,8 +4,7 @@ const router = express.Router();
 
 const signUpLoginController = require("../controllers/signUpLogin.controller")
 
-router.post('/patients/signup', signUpLoginController.patientSignUp);
-router.post('/nurses/signup', signUpLoginController.nurseSignUp);
-router.post('/signin', signUpLoginController.signIn);
+router.post('/auth/register', signUpLoginController.Signup);
+router.post('/auth/login', signUpLoginController.signIn);
 
 module.exports = router;
