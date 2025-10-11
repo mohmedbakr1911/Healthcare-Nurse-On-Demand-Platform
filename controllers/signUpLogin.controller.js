991f4c458054e52c5fe54efff211a2cb5bfb6052
Sohaib <sohibhassan827@gmail.com>
2025-10-11 11:00:02 +0300

    update signUpLogin.controller with prisma query

--- controllers/signUpLogin.controller.js
+++ controllers/signUpLogin.controller.js
@@ -27,11 +27,7 @@ const Signup = asyncWrapper(async (req, res, next) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const verifictionCode = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString();
-
-  const expired_code_at = new Date(Date.now() + 10 * 60 * 1000);
+  const { expired_code_at, verifictionCode } = getVerificationCode();
 
   // const newUser = await pool.query(
   //   "INSERT INTO users (email, phone, user_type, provider) VALUES ($1, $2, $3, $4) RETURNING *",
@@ -39,15 +35,21 @@ const Signup = asyncWrapper(async (req, res, next) => {
   // );
 
   const newUser = await prisma.users.create({
-    data:{email, phone, user_type, provider: "local"}
+    data: { email, phone, user_type, provider: "local" },
   });
 
   // const userCredentials = await pool.query(
   //   "INSERT INTO credentials (user_id, password_hash, verification_code, code_expires_at) VALUES ($1, $2, $3, $4) RETURNING *",
   //   [newUser.rows[0].id, hashedPassword, verifictionCode, expired_code_at]
   // );
-  const userCredentials = await prisma.credentials.create({
-    data:{user_id: newUser.id, password_hash: hashedPassword, verification_code: verifictionCode, code_expires_at: expired_code_at}
+
+  await prisma.credentials.create({
+    data: {
+      user_id: newUser.id,
+      password_hash: hashedPassword,
+      verification_code: verifictionCode,
+      code_expires_at: expired_code_at,
+    },
   });
 
   await sendVerificationEmail(email, verifictionCode);
@@ -59,7 +61,7 @@ const Signup = asyncWrapper(async (req, res, next) => {
   return res.status(201).json({
     status: httpStatusText.SUCCESS,
     data: {
-      user_id: newUser.rows[0].id,
+      user_id: newUser.id,
     },
   });
 });
@@ -75,8 +77,8 @@ const signIn = asyncWrapper(async (req, res, next) => {
   // );
 
   const result = await prisma.users.findUnique({
-    where:{email: email},
-    include: {credentials: true}
+    where: { email: email },
+    include: { credentials: true },
   });
 
   if (!result) {
@@ -86,7 +88,7 @@ const signIn = asyncWrapper(async (req, res, next) => {
   }
   const passwordMatch = await bcrypt.compare(
     password,
-    result.rows[0].password_hash
+    result.credentials.password_hash
   );
 
   if (!passwordMatch) {
@@ -95,11 +97,9 @@ const signIn = asyncWrapper(async (req, res, next) => {
     );
   }
 
-  if (result.rows[0].status !== "active") {
-    const verifictionCode = Math.floor(
-      100000 + Math.random() * 900000
-    ).toString();
-    const expired_code_at = new Date(Date.now() + 10 * 60 * 1000);
+  if (result.credentials.status !== "active") {
+    const { expired_code_at, verifictionCode } = getVerificationCode();
+
     // await pool.query(
     //   `UPDATE credentials
     //   SET verification_code = $1,
@@ -111,8 +111,11 @@ const signIn = asyncWrapper(async (req, res, next) => {
     // );
 
     await prisma.credentials.update({
-      where:{user_id: result.id},
-      data: {verification_code: verifictionCode, code_expires_at: expired_code_at}
+      where: { user_id: result.id },
+      data: {
+        verification_code: verifictionCode,
+        code_expires_at: expired_code_at,
+      },
     });
 
     await sendVerificationEmail(email, verifictionCode);
@@ -121,7 +124,7 @@ const signIn = asyncWrapper(async (req, res, next) => {
     );
   }
 
-  const token = tokenMiddleware.generateToken(result.rows[0]);
+  const token = tokenMiddleware.generateToken(result);
 
   return res.status(200).json({
     status: httpStatusText.SUCCESS,
@@ -135,30 +138,71 @@ const signIn = asyncWrapper(async (req, res, next) => {
 const verifyEmail = asyncWrapper(async (req, res, next) => {
   const { email, code } = req.body;
 
-  const user = await pool.query(
-    `SELECT u.*
-      FROM users u
-      JOIN credentials c ON u.id = c.user_id
-      WHERE u.email = $1
-      AND c.verification_code = $2
-      AND c.code_expires_at > NOW()`,
-    [email, code]
-  );
+  // const user = await pool.query(
+  //   `SELECT u.*
+  //     FROM users u
+  //     JOIN credentials c ON u.id = c.user_id
+  //     WHERE u.email = $1
+  //     AND c.verification_code = $2
+  //     AND c.code_expires_at > NOW()`,
+  //   [email, code]
+  // );
 
-  
+  const user = await prisma.users.findUnique({
+    where: { email },
+    include: {
+      credentials: {
+        where: {
+          verification_code: String(code),
+        },
+      },
+    },
+  });
 
-  if (!user.rows.length) {
+  if (!user || !user.credentials) {
     return next(
-      appError.create("Invalid or expired code", 400, httpStatusText.FAIL)
+      appError.create("Invalid email or code", 400, httpStatusText.FAIL)
     );
   }
 
-  await pool.query(
-    "UPDATE credentials SET status = 'active', verification_code = NULL, code_expires_at = NULL WHERE user_id = $1",
-    [user.rows[0].id]
-  );
+  const expirationTime = user.credentials.code_expires_at;
 
-  const token = tokenMiddleware.generateToken(user.rows[0]);
+  if (!(expirationTime && expirationTime.getTime() > new Date().getTime())) {
+    const { expired_code_at, verifictionCode } = getVerificationCode();
+
+    await prisma.credentials.update({
+      where: { user_id: user.id },
+      data: {
+        verification_code: verifictionCode,
+        code_expires_at: expired_code_at,
+      },
+    });
+    await sendVerificationEmail(email, verifictionCode);
+
+    return next(
+      appError.create(
+        "Code has expired chek the new code",
+        400,
+        httpStatusText.FAIL
+      )
+    );
+  }
+
+  // await pool.query(
+  //   "UPDATE credentials SET status = 'active', verification_code = NULL, code_expires_at = NULL WHERE user_id = $1",
+  //   [user.rows[0].id]
+  // );
+
+  await prisma.credentials.update({
+    where: { user_id: user.id },
+    data: {
+      status: "active",
+      verification_code: null,
+      code_expires_at: null,
+    },
+  });
+
+  const token = tokenMiddleware.generateToken(user);
 
   res
     .status(200)
@@ -168,19 +212,27 @@ const verifyEmail = asyncWrapper(async (req, res, next) => {
 const callback = asyncWrapper(async (req, res) => {
   const email = req.user.emails[0].value;
 
-  const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-    email,
-  ]);
-  if (user.rows.length === 0) {
-    const newUser = await pool.query(
-      "INSERT INTO users (email, provider) VALUES ($1, $2) RETURNING *",
-      [email, "google"]
-    );
-    const token = tokenMiddleware.generateToken(newUser.rows[0]);
+  // const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+  //   email,
+  // ]);
+
+  const user = await prisma.users.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    // const newUser = await pool.query(
+    //   "INSERT INTO users (email, provider) VALUES ($1, $2) RETURNING *",
+    //   [email, "google"]
+    // );
+    const newUser = await prisma.users.create({
+      data: { email, provider: "google" },
+    });
+    const token = tokenMiddleware.generateToken(newUser);
     res.redirect(`${process.env.FRONT_URL}/completeData?token=${token}`);
   }
 
-  const token = tokenMiddleware.generateToken(user.rows[0]);
+  const token = tokenMiddleware.generateToken(user);
   res.redirect(`${process.env.FRONT_URL}?token=${token}`);
 });
 
@@ -204,24 +256,49 @@ const completeData = asyncWrapper(async (req, res, next) => {
 
 const resetPasswordRequest = asyncWrapper(async (req, res, next) => {
   const { email } = req.body;
-  const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-    email,
-  ]);
+  // const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+  //   email,
+  // ]);
 
-  if (user.rows.length === 0) {
+  const user = await prisma.users.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
     return next(appError.create("User not found", 404, httpStatusText.FAIL));
   }
 
-  const resetToken = tokenMiddleware.generateToken(user.rows[0]);
+  const credential = await prisma.credentials.findUnique({
+    where: { user_id: user.id },
+  });
+  if (!credential || credential.status !== "active") {
+    return next(
+      appError.create("User not found or inactive", 404, httpStatusText.FAIL)
+    );
+  }
+
+  const resetToken = tokenMiddleware.generateToken(user);
   await sendResetPasswordEmail(email, resetToken);
 
-  await pool.query(
-    `UPDATE credentials
-    SET reset_token = $1, reset_token_expires_at = $2
-    WHERE user_id = $3`,
-    [resetToken, new Date(Date.now() + 10 * 60 * 1000), user.rows[0].id]
-  );
+  // await pool.query(
+  //   `UPDATE credentials
+  //   SET reset_token = $1, reset_token_expires_at = $2
+  //   WHERE user_id = $3`,
+  //   [resetToken, new Date(Date.now() + 10 * 60 * 1000), user.id]
+  // );
+  const updated = await prisma.credentials.update({
+    where: { user_id: user.id },
+    data: {
+      reset_token: resetToken,
+      reset_token_expires_at: new Date(Date.now() + 10 * 60 * 1000),
+    },
+  });
 
+  if (!updated) {
+    return next(
+      appError.create("Failed to set reset token", 500, httpStatusText.FAIL)
+    );
+  }
   return res.status(200).json({
     status: httpStatusText.SUCCESS,
     message: "Reset password email sent",
@@ -243,34 +320,69 @@ const resetPassword = asyncWrapper(async (req, res, next) => {
     );
   }
 
-  const credential = await pool.query(
-    `SELECT * FROM credentials
-    WHERE user_id = $1
-    AND reset_token = $2
-    AND reset_token_expires_at > NOW()`,
-    [req.currentUser.id, req.token]
-  );
-  if (credential.rows.length === 0) {
+  // const credential = await pool.query(
+  //   `SELECT * FROM credentials
+  //   WHERE user_id = $1
+  //   AND reset_token = $2
+  //   AND reset_token_expires_at > NOW()`,
+  //   [req.currentUser.id, req.token]
+  // );
+
+  const credential = await prisma.credentials.findFirst({
+    where: {
+      user_id: req.currentUser.id,
+      reset_token: req.token,
+      reset_token_expires_at: {
+        gt: new Date(),
+      },
+    },
+  });
+
+  if (!credential) {
     return next(
       appError.create("Invalid or expired token", 400, httpStatusText.FAIL)
     );
   }
 
   const hashedPassword = await bcrypt.hash(new_password, 10);
-  await pool.query(
-    `UPDATE credentials
-    SET password_hash = $1,
-      reset_token = NULL,
-      reset_token_expires_at = NULL
-   WHERE user_id = $2`, // ONLY condition for filtering
-    [hashedPassword, req.currentUser.id]
-  );
+  // await pool.query(
+  //   `UPDATE credentials
+  //   SET password_hash = $1,
+  //     reset_token = NULL,
+  //     reset_token_expires_at = NULL
+  //  WHERE user_id = $2`, // ONLY condition for filtering
+  //   [hashedPassword, req.currentUser.id]
+  // );
+
+  const updated = await prisma.credentials.update({
+    where: { user_id: req.currentUser.id },
+    data: {
+      password_hash: hashedPassword,
+      reset_token: null,
+      reset_token_expires_at: null,
+    },
+  });
+
+  if (!updated) {
+    return next(
+      appError.create("Failed to reset password", 500, httpStatusText.FAIL)
+    );
+  }
+
   return res.status(200).json({
     status: httpStatusText.SUCCESS,
     message: "Password has been reset successfully",
   });
 });
 
+const getVerificationCode = () => {
+  const verifictionCode = Math.floor(
+    100000 + Math.random() * 900000
+  ).toString();
+  const expired_code_at = new Date(Date.now() + 10 * 60 * 1000);
+
+  return { verifictionCode, expired_code_at };
+};
 module.exports = {
   signIn,
   Signup,
