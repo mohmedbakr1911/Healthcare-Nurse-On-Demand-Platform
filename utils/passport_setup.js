401fb44d9683c7663eead57ab6112585a5c3cbe7
Sohaib <sohibhassan827@gmail.com>
2025-10-11 12:18:05 +0300

    run google on server

--- utils/passport_setup.js
+++ utils/passport_setup.js
@@ -4,7 +4,8 @@ module.exports = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL:
+      "https://healthcarenurseondemand.vercel.app/api/auth/google/callback",
   },
   async (accessToken, refreshToken, profile, done) => {
     // Here, you find or create the user in your DB
