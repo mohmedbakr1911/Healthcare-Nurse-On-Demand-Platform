be2de7f740211d7fb43fb141d4851d5c07bee731
Sohaib <sohibhassan827@gmail.com>
2025-10-12 21:35:17 +0300

    try to fix 2

--- utils/passport_setup.js
+++ utils/passport_setup.js
@@ -5,7 +5,7 @@ module.exports = new GoogleStrategy(
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL:
-      "https://healthcarenurseondemand.vercel.app/api/users/google/callback",
+      "https://healthcarenurseondemand.vercel.app/api/auth/google/callback",
   },
   (accessToken, refreshToken, profile, done) => {
     // Here, you find or create the user in your DB
